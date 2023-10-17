907a6ee6df7b12b4665570e7acd198507be05ca3
GirtsFreimanis <girtslp@gmail.com>
2023-10-17 15:32:41 +0300

    tidy up

--- company-registry/company-registry.php
+++ company-registry/company-registry.php
@@ -15,9 +15,12 @@ class CompanyApplication
             exit;
         }
         $search = str_replace(" ", "%20", $search);
-        $apiData = json_decode(file_get_contents("https://data.gov.lv/dati/lv/api/3/action/datastore_search?q=" . $search . "&resource_id=25e80bf3-f107-4ab4-89ef-251b5b9374e9"));
+        $apiData = json_decode(file_get_contents(
+            "https://data.gov.lv/dati/lv/api/3/action/datastore_search?q="
+            . $search . "&resource_id=25e80bf3-f107-4ab4-89ef-251b5b9374e9"));
         if (empty($apiData->result->records)) {
             echo "No companies found!\n";
+            exit;
         }
         $this->initialize($apiData);
         $this->print();
@@ -33,15 +36,14 @@ class CompanyApplication
         /* @var Company $company */
         foreach ($this->companies->getCompanies() as $company) {
             echo str_repeat("=", 25) . "\n";
-            echo "name: " . $company->getName() . "\n";
-            echo "type: " . $company->getType() . "\n";
-            echo "regcode: " . $company->getRegcode() . "\n";
-            echo "address: " . $company->getAddress() . ", LV-{$company->getIndex()}\n";
-            echo "registered: " . $company->getRegistered() . "\n";
+            echo "Name: " . $company->getName() . "\n";
+            echo "Type: " . $company->getType() . "\n";
+            echo "Reg. code: " . $company->getRegcode() . "\n";
+            echo "Address: " . $company->getAddress() . ", LV-{$company->getIndex()}\n";
+            echo "Registered: " . $company->getRegistered() . "\n";
         }
     }
 }
-
 
 $app = new CompanyApplication();
 $app->run();
